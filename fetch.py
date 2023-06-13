f0a38f8dc29cb76bedde80e3e4be16946fdcc29a
Adeela Ashraf <adeela-97@hotmail.com>
2023-06-14 00:26:19 +0200

    Annotation + Conversion to JSON + Non-working GeoTIFF

--- fetch.py
+++ fetch.py
@@ -1,6 +1,12 @@
 from webdav3.client import Client
 from configparser import ConfigParser
+import glob
+import os
+import json
+import rasterio
 
+
+## 1. Retrieve files
 # Read the configuration file
 config = ConfigParser()
 config.read('config.ini')
@@ -19,6 +25,41 @@ options = {
 }
 client = Client(options)
 
-# Download the file from the remote server
 local_file_path = './geotiffs/'
+output_file_path = 'data.json'
+
+# Download the file from the remote server
 client.download(remote_path=remote_file_path, local_path=local_file_path)
+
+## 2. Calculate extens files
+def calculate_extents(directory):
+    file_list = glob.glob(directory + '/**/*.tif', recursive=True)
+    min_x = float('inf')
+    min_y = float('inf')
+    max_x = float('-inf')
+    max_y = float('-inf')
+    
+    for file_path in file_list:
+        with rasterio.open(file_path) as dataset:
+            bounds = dataset.bounds
+            min_x = min(min_x, bounds.left)
+            min_y = min(min_y, bounds.bottom)
+            max_x = max(max_x, bounds.right)
+            max_y = max(max_y, bounds.top)
+
+    return min_x, min_y, max_x, max_y
+
+min_x, min_y, max_x, max_y = calculate_extents(local_file_path)
+print("Minimum Extents:", min_x, min_y)
+print("Maximum Extents:", max_x, max_y)
+
+json_dict = {}
+## 3. Save to data.json
+json_dict['file_names'] = glob.glob(local_file_path + '/**/*.tif', recursive=True)
+json_dict['min_x'] = min_x
+json_dict['min_y'] = min_y
+json_dict['max_x'] = max_x
+json_dict['max_y'] = max_y
+
+with open(output_file_path, 'w') as json_file:
+    json.dump(json_dict, json_file)
--- main.js
+++ main.js
@@ -1,24 +1,82 @@
 import "./style.css";
-import * as ol from "ol";
-import {Map, View} from "ol";
+import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
-import {
-    DragRotateAndZoom,
-    defaults as defaultInteractions,
-} from "ol/interaction.js";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import ImageLayer from "ol/layer/Image";
+import ImageStatic from "ol/source/ImageStatic";
+import { getCenter } from "ol/extent";
+
+// Create a vector source and layer for drawing
+const vectorSource = new VectorSource({
+  wrapX: false,
+});
+const editLayer = new VectorLayer({
+  source: vectorSource,
+});
 
 const map = new Map({
- interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
   target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
+    editLayer, // Add the vector layer to the map
   ],
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 2,
+  }),
 });
 
+var editor = new ole.Editor(map);
+
+var cad = new ole.control.CAD({
+  source: editLayer.getSource(),
+});
+
+var draw = new ole.control.Draw({
+  source: editLayer.getSource(),
+});
+
+var drawLine = new ole.control.Draw({
+  type: 'LineString',
+  source: editLayer.getSource(),
+});
+
+var drawPoly = new ole.control.Draw({
+  type: 'Polygon',
+  source: editLayer.getSource(),
+});
+
+editor.addControls([draw, cad, drawLine, drawPoly]);
+
+// Read TIFF file names and extent from data.json
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const { file_names, min_x, min_y, max_x, max_y } = data;
+
+    // Create image layers for each TIFF file
+    file_names.forEach((file) => {
+      const imageLayer = new TileLayer({
+        source: new ImageStatic({
+          url: file,
+          imageExtent: [min_x, min_y, max_x, max_y],
+        }),
+      });
+      map.addLayer(imageLayer);
+    });
+
+    // Calculate center and zoom based on the TIFF file extent
+    const extent = [min_x, min_y, max_x, max_y];
+    const center = getCenter(extent);
+    const zoom = map.getView().getZoomForResolution(
+      map.getView().getResolutionForExtent(extent)
+    );
+
+    // Update the view to center the map around the TIFF files
+    map.getView().setCenter(center);
+    map.getView().setZoom(zoom);
+  });
