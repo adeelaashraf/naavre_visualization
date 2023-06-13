11593b50eb4659a72db451989085beb463eb64c3
Adeela Ashraf <adeela-97@hotmail.com>
2023-06-13 20:27:35 +0200

    Simple workflow running (no NodePort)

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import './style.css';
-import * as ol from 'ol';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import * as ol from "ol";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
 import {
     DragRotateAndZoom,
     defaults as defaultInteractions,
-} from 'ol/interaction.js';
+} from "ol/interaction.js";
 
 const map = new Map({
-  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
-  target: 'map',
+ interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+  target: "map",
   layers: [
     new TileLayer({
       source: new OSM()
